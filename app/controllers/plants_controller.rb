dc0aa62db8db08bb9ca778eee10c7e044423169d
antoninaorobets <pani.antonina.don@gmail.com>
2022-06-07 13:21:55 -0400

    commit tree 7ab52776c17c7478ab059cb48b393c6356ed185f

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :rescue_method
   # GET /plants
   def index
     plants = Plant.all
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = Plant.find(params[:id])
     render json: plant
   end
 
@@ -18,7 +18,23 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
   private
+  def rescue_method
+    render json: {error: "error"}, status: :not_found
+
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
